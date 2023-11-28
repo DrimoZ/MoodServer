35bb6f5af92a4232840cc3cc5942edb512f61a25
DrimoZ <drimozbe@gmail.com>
2023-11-28 14:59:07 +0100

    Edit Database to match Domain Requirement (Rebuild Docker Image)

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -8,24 +8,24 @@ GO
 CREATE TABLE accounts (
     acc_id INT PRIMARY KEY,
     acc_phone_number NVARCHAR(255),
-    acc_birth_date DATE not null,
+    acc_birth_date DATE NOT NULL,
     acc_description NVARCHAR(255)
 );
 
 CREATE TABLE users (
     user_id INT PRIMARY KEY,
-    user_mail NVARCHAR(255) not null,
-    user_login NVARCHAR(255) UNIQUE not null,
-    user_name NVARCHAR(255) not null,
-    user_password NVARCHAR(255) not null,
-    user_role INT not null,
+    user_mail NVARCHAR(255) NOT NULL,
+    user_login NVARCHAR(255) UNIQUE NOT NULL,
+    user_name NVARCHAR(255) NOT NULL,
+    user_password NVARCHAR(255) NOT NULL,
+    user_role INT NOT NULL,
     user_title NVARCHAR(255),
     acc_id INT FOREIGN KEY REFERENCES accounts(acc_id)
 );
 
 CREATE TABLE friends (
-    acc_id INT FOREIGN KEY REFERENCES accounts(acc_id) not null,
-    friend_id INT FOREIGN KEY REFERENCES accounts(acc_id) not null
+    user_id INT FOREIGN KEY REFERENCES users(user_id),
+    friend_id INT FOREIGN KEY REFERENCES users(user_id)
 );
 
 CREATE TABLE groups (
@@ -35,18 +35,18 @@ CREATE TABLE groups (
 
 CREATE TABLE user_groups (
     user_group_id INT PRIMARY KEY,
-    user_id INT FOREIGN KEY REFERENCES users(user_id) not null,
-    group_id INT FOREIGN KEY REFERENCES groups(group_id) not null
+    user_id INT FOREIGN KEY REFERENCES users(user_id),
+    group_id INT FOREIGN KEY REFERENCES groups(group_id)
 );
 
 CREATE TABLE messages (
     msg_id INT PRIMARY KEY,
-    msg_date DATETIME not null,
+    msg_date DATETIME NOT NULL,
 );
 
 CREATE TABLE text_messages (
     txtMsg_id INT PRIMARY KEY,
-    txtMsg_content NVARCHAR(255) not null,
+    txtMsg_content NVARCHAR(255) NOT NULL,
     user_id INT FOREIGN KEY REFERENCES user_groups(user_group_id),
     msg_id INT FOREIGN KEY REFERENCES messages(msg_id)
 );
@@ -60,42 +60,43 @@ CREATE TABLE friend_request_messages (
 
 CREATE TABLE publications (
     pub_id INT PRIMARY KEY,
-    pub_content NVARCHAR(255) not null,
+    pub_content NVARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE photo_publications (
     photoPub_id INT PRIMARY KEY,
-    photoPub_extention NVARCHAR(16),
-    photoPub_content NVARCHAR(255),
+    photoPub_extention NVARCHAR(16) NOT NULL,
+    photoPub_content NVARCHAR(255) NOT NULL,
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
 );
 
 CREATE TABLE video_publications (
     videoPub_id INT PRIMARY KEY,
-    videoPub_extention NVARCHAR(16),
-    videoPub_content NVARCHAR(255),
+    videoPub_extention NVARCHAR(16) NOT NULL,
+    videoPub_content NVARCHAR(255) NOT NULL,
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
 );
 
 CREATE TABLE survey_publications (
     surveyPub_id INT PRIMARY KEY,
-    surveyPub_content NVARCHAR(255),
+    surveyPub_content NVARCHAR(255 NOT NULL),
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
 );
 
 CREATE TABLE comments (
     cmt_id INT PRIMARY KEY,
-    cmt_date DATETIME,
-    cmt_content NVARCHAR(255),
+    cmt_date DATETIME NOT NULL,
+    cmt_content NVARCHAR(255) NOT NULL,
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id),
-    acc_id INT FOREIGN KEY REFERENCES users(user_id)
+    user_id INT FOREIGN KEY REFERENCES users(user_id)
 );
 
 CREATE TABLE likes (
     like_id INT PRIMARY KEY,
-    like_date DATETIME,
-    like_apreciation INT,
-    pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
+    like_date DATETIME NOT NULL,
+    like_type INT NOT NULL,
+    pub_id INT FOREIGN KEY REFERENCES publications(pub_id),
+    user_id INT FOREIGN KEY REFERENCES users(user_id)
 );
 
 GO
