db4e6165389735c561484b7397dca3a9b36754b4
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-11-28 17:00:41 +0100

    Add id to DbAccount in Mapper.cs

--- Application/Mapper.cs
+++ Application/Mapper.cs
@@ -1,13 +1,17 @@
+using System.ComponentModel.Design;
 using Application.Dtos.Account;
 using Application.Dtos.Publication;
 using Application.Dtos.User;
 using AutoMapper;
 using Infrastructure.EntityFramework.DbEntities;
+using Microsoft.Identity.Client;
+using WebApi.Services;
 
 namespace Application;
 
 public class Mapper: Profile
 {
+    private readonly IdService _idService = new IdService();
     public Mapper()
     {
         //Users
@@ -16,8 +20,8 @@ public class Mapper: Profile
         
         //Account
         CreateMap<DbAccount, DtoOutputAccount>();
-        CreateMap<DtoInputAccount, DbAccount>();
-        
+        CreateMap<DtoInputAccount, DbAccount>()
+            .BeforeMap((s, d) => d.Id = _idService.GenerateRandomId(32) );
         
         //Publication
         CreateMap<DbPublication, DtoOutputPublication>();
