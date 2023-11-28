af7c115fc136e3d773a04e126e32eac73ab26afc
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-11-28 15:53:52 +0100

    Edition Variable DtoOutputAccount.cs

--- Application/Dtos/Account/DtoOutputAccount.cs
+++ Application/Dtos/Account/DtoOutputAccount.cs
@@ -5,5 +5,5 @@ public class DtoOutputAccount
     public int Id { get; set; }
     public string PhoneNumber { get; set; }
     public string Description { get; set; }
-    public DateOnly BirthDate { get; set; }
+    public DateTime BirthDate { get; set; }
 }
--- Application/Dtos/User/DtoInputUser.cs
+++ Application/Dtos/User/DtoInputUser.cs
@@ -2,12 +2,9 @@ namespace Application.Dtos.User;
 
 public class DtoInputUser
 {
-    public int Id { get; set; }
     public string Mail { get; set; }
     public string Login { get; set; }
     public string Name { get; set; }
     public string Password { get; set; }
-    public int Role { get; set; }
-    public string Title { get; set; }
     public int AccountId { get; set; }
 }
