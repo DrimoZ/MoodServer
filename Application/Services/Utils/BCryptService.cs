abf0d44fc783f385c0c039445168ddcf008b143c
DrimoZ <drimozbe@gmail.com>
2023-11-30 13:56:01 +0100

    Add Conditions to Input Dtos

--- Application/Services/Utils/BCryptService.cs
+++ Application/Services/Utils/BCryptService.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Application.Services.Utils;
 
 public class BCryptService
@@ -10,6 +12,37 @@ public class BCryptService
     public bool VerifyPassword(string password, string hashedPassword)
     {
         return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+    
+    private static bool ValidatePassword(string password)
+    {
+        var hasNumber = new Regex(@"[0-9]+");
+        var hasUpperChar = new Regex(@"[A-Z]+");
+        var hasLowerChar = new Regex(@"[a-z]+");
+        var hasMinimum8Chars = new Regex(@".{8,}");
+        var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+        if (!hasLowerChar.IsMatch(password))
+        {
+            throw new ArgumentException("Password should contain at least one lower case letter.");
+        }
+        if (!hasUpperChar.IsMatch(password))
+        {
+            throw new ArgumentException("Password should contain at least one upper case letter.");
+        }
+        if (!hasNumber.IsMatch(password))
+        {
+            throw new ArgumentException("Password should contain at least one numeric value.");
+        }
+        if (!hasSymbols.IsMatch(password))
+        {
+            throw new ArgumentException("Password should contain at least one special case characters.");
+        }
+        if (!hasMinimum8Chars.IsMatch(password))
+        {
+            throw new ArgumentException("Password should not be less than 8 characters.");
+        }
+        return true;
     }
 }
 
