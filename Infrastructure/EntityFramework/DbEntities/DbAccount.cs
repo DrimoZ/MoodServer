fcfd98b5706176e5384e60227565561b52b4ddbd
DrimoZ <drimozbe@gmail.com>
2023-11-30 12:04:30 +0100

    Edit DbAccount + DbUser to allow null in strings for Title Description etc

--- Infrastructure/EntityFramework/DbEntities/DbAccount.cs
+++ Infrastructure/EntityFramework/DbEntities/DbAccount.cs
@@ -3,7 +3,7 @@ namespace Infrastructure.EntityFramework.DbEntities;
 public class DbAccount
 {
     public string Id { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Description { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Description { get; set; }
     public DateTime BirthDate { get; set; }
 }
--- Infrastructure/EntityFramework/DbEntities/DbUser.cs
+++ Infrastructure/EntityFramework/DbEntities/DbUser.cs
@@ -8,6 +8,6 @@ public class DbUser
     public string Name { get; set; }
     public string Password { get; set; }
     public int Role { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public string AccountId { get; set; }
 }
