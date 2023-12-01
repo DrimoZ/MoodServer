cc4a098dafb96d9b31750afb33f9de60aa4487fb
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-01 18:26:20 +0100

    binding Context(group)

--- Application/UseCases/Groups/UseCaseCreateGroup.cs
+++ Application/UseCases/Groups/UseCaseCreateGroup.cs
@@ -1,6 +1,9 @@
+using Application.UseCases.Utils;
+using Infrastructure.EntityFramework.DbEntities;
+
 namespace Application.UseCases.Groups;
 
-public class UseCaseCreateGroup
+public class UseCaseCreateGroup:IUseCaseWriter<DbGroup, DtoInputCreateGroup>
 {
     
 }
--- Infrastructure/EntityFramework/DbEntities/DbGroup.cs
+++ Infrastructure/EntityFramework/DbEntities/DbGroup.cs
@@ -2,5 +2,7 @@ namespace Infrastructure.EntityFramework.DbEntities;
 
 public class DbGroup
 {
-    
+    public int Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public string Name { get; set; }
 }
