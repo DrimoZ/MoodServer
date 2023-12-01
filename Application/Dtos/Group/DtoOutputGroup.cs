d4f899db7ba5d9e42f91e78f2c196057d5d789fb
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-01 18:40:17 +0100

    create DtoOutputGroup.cs

---
+++ Application/Dtos/Group/DtoOutputGroup.cs
@@ -0,0 +1,8 @@
+namespace Application.Dtos.Group;
+
+public class DtoOutputGroup
+{
+    public int Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public string Name { get; set; }
+}
