ef9cde87bca6edbd56d8c69dd8abbe5ed9f568df
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-02 19:42:54 +0100

    put attribute DbMessage.cs

--- Infrastructure/EntityFramework/DbEntities/DbMessage.cs
+++ Infrastructure/EntityFramework/DbEntities/DbMessage.cs
@@ -2,5 +2,8 @@ namespace Infrastructure.EntityFramework.DbEntities;
 
 public class DbMessage
 {
-    
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int UserGroupId { get; set; }
+    public int CommId { get; set; }
 }
