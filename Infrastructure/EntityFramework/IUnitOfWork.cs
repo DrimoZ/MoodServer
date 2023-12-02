d9e8be0692c495f3c396bb7565a5c255ae7578dc
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-02 17:21:18 +0100

    Create Interface For UnitOfWork

---
+++ Infrastructure/EntityFramework/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFramework;
+
+public interface IUnitOfWork<out TContext> where TContext : DbContext, new()
+{
+    //The following Property is going to hold the context object
+    TContext Context { get; }
+    //Start the database Transaction
+    void CreateTransaction();
+    //Commit the database Transaction
+    void Commit();
+    //Rollback the database Transaction
+    void Rollback();
+    //DbContext Class SaveChanges method
+    void Save();
+}
