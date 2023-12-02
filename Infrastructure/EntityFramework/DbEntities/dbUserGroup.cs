4567a308d2eca8b44ab2b2aa472a235370f8527a
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-02 17:17:37 +0100

    Create dbUserGroup.cs which is a table from db

---
+++ Infrastructure/EntityFramework/DbEntities/dbUserGroup.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Infrastructure.EntityFramework.DbEntities;
+
+public class dbUserGroup
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public int GroupId { get; set; }
+}
