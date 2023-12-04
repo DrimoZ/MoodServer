c52f1b715a74b3f4ad7b06d140e5b9b5ef9bdcff
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-04 21:28:09 +0100

    create dtoInput and output message

---
+++ Application/Dtos/Message/DtoInputMessage.cs
@@ -0,0 +1,7 @@
+namespace Application.Dtos.Message;
+
+public class DtoInputMessage
+{
+    public string Content { get; set; }
+    public int UserGroupId { get; set; }
+}
---
+++ Application/Dtos/Message/DtoOutputMessage.cs
@@ -0,0 +1,9 @@
+namespace Application.Dtos.Message;
+
+public class DtoOutputMessage
+{
+    public int Id { get; set; }
+    public string Content { get; set; }
+    public int UserGroupId { get; set; }
+    public int CommId { get; set; }
+}
