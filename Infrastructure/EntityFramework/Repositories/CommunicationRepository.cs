87754c54cc05e398f8e7d7d7c4bc0b6a7f3655f5
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-04 21:21:37 +0100

    Create Repositories for CommunicationRepository.cs

--- Infrastructure/EntityFramework/Repositories/CommunicationRepository.cs
+++ Infrastructure/EntityFramework/Repositories/CommunicationRepository.cs
@@ -1,6 +1,19 @@
+using Infrastructure.EntityFramework.DbEntities;
+
 namespace Infrastructure.EntityFramework.Repositories;
 
-public class CommunicationRepository
+public class CommunicationRepository:ICommunicationRepository
 {
-    
+    private readonly MoodContext _context;
+
+    public CommunicationRepository(MoodContext context)
+    {
+        _context = context;
+    }
+
+    public DbCommunication Create(DbCommunication com)
+    {
+        _context.Communications.Add(com);
+        return com;
+    }
 }
--- Infrastructure/EntityFramework/Repositories/ICommunicationRepository.cs
+++ Infrastructure/EntityFramework/Repositories/ICommunicationRepository.cs
@@ -1,6 +1,8 @@
+using Infrastructure.EntityFramework.DbEntities;
+
 namespace Infrastructure.EntityFramework.Repositories;
 
 public interface ICommunicationRepository
 {
-    
+    public DbCommunication Create(DbCommunication com);
 }
