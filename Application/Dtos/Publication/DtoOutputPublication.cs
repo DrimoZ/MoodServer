062ccf9ea0c72d6bd794e6641c6708004ca6bc81
XBanditxPandaX <108540082+XBanditxPandaX@users.noreply.github.com>
2023-12-08 15:33:12 +0100

    Modified DtoOutputPublication.cs

--- Application/Dtos/Publication/DtoOutputPublication.cs
+++ Application/Dtos/Publication/DtoOutputPublication.cs
@@ -6,5 +6,26 @@ public class DtoOutputPublication
     public string UserId { get; set; }
     public string Content { get; set; }
     public DateTime date { get; set; }
-    public bool IsDeleted { get; set; }
+
+    public IEnumerable<DtoLike> likes;
+    public IEnumerable<DtoPhotoPublication> photoPublications;
+    public DtoVideoPublication videoPublications;
+
+    public class DtoLike
+    {
+        public int Type { get; set; }
+        public int Number { get; set; }
+    }
+    
+    public class DtoPhotoPublication
+    {
+        public string Extension { get; set; }
+        public string Photo { get; set; }
+    }
+    
+    public class DtoVideoPublication
+    {
+        public string Extension { get; set; }
+        public string Video { get; set; }
+    }
 }
