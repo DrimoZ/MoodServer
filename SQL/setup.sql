efa6c7adecb9023eb2e6451c96f51e80c2327ff5
AkiSekai <119940894+AkiSekai@users.noreply.github.com>
2023-12-09 16:32:21 +0100

    Update setup.sql

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -67,34 +67,26 @@ CREATE TABLE friend_requests (
 CREATE TABLE publications (
     pub_id INT PRIMARY KEY IDENTITY,
     pub_content NVARCHAR(255) NOT NULL,
-    pub_date DATETIME NOT NULL,
+    pub_date DATETIME DEFAULT (GETDATE()),
     pub_isDeleted BIT NOT NULL DEFAULT 0,
     user_id CHAR(32) FOREIGN KEY REFERENCES users(user_id)
 );
 
-CREATE TABLE photo_publications (
-    photoPub_id INT PRIMARY KEY IDENTITY,
-    photoPub_extention NVARCHAR(16) NOT NULL,
-    photoPub_content NVARCHAR(255) NOT NULL,
-    pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
+CREATE TABLE images (
+    img_id INT PRIMARY KEY IDENTITY,
+    img_path NVARCHAR(MAX) NOT NULL,
+    img_date DATETIME DEFAULT (GETDATE())
 );
 
-CREATE TABLE video_publications (
-    videoPub_id INT PRIMARY KEY IDENTITY,
-    videoPub_extention NVARCHAR(16) NOT NULL,
-    videoPub_content NVARCHAR(255) NOT NULL,
-    pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
-);
-
-CREATE TABLE survey_publications (
-    surveyPub_id INT PRIMARY KEY IDENTITY,
-    surveyPub_content NVARCHAR(255) NOT NULL,
+CREATE TABLE publication_elements (
+    elmt_id INT PRIMARY KEY IDENTITY,
+    img_id INT FOREIGN KEY REFERENCES images(img_id),
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id)
 );
 
 CREATE TABLE comments (
     cmt_id INT PRIMARY KEY IDENTITY,
-    cmt_date DATETIME NOT NULL,
+    cmt_date DATETIME DEFAULT (GETDATE()),
     cmt_content NVARCHAR(255) NOT NULL,
     cmt_isDeleted BIT NOT NULL DEFAULT 0,
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id),
@@ -103,11 +95,12 @@ CREATE TABLE comments (
 
 CREATE TABLE likes (
     like_id INT PRIMARY KEY IDENTITY,
-    like_date DATETIME NOT NULL,
+    like_date DATETIME DEFAULT (GETDATE()),
     like_type INT NOT NULL,
     pub_id INT FOREIGN KEY REFERENCES publications(pub_id),
     user_id CHAR(32) FOREIGN KEY REFERENCES users(user_id)
 );
+
 
 GO
 INSERT INTO accounts VALUES ('acc_ku784AJalJIkPxQPM6QeLyuPgrno', '0600000000', '1990-01-01', 'Compte 1');
@@ -115,13 +108,13 @@ INSERT INTO accounts VALUES ('acc_f7Dtuqn32rAp7ocsonMiK6MAkOoE', '0611111111', '
 INSERT INTO accounts VALUES ('acc_caTaXKCD7D9AgGAQxT9kPOxxxIna', '0633333333', '2000-01-01', 'Yo moi cest mod3 : ) ');
 INSERT INTO accounts VALUES ('acc_8vLHdpRjrqF_RRM2NJcVEFqmZkGt', '0633333333', '2003-01-01', 'Je stream tous les jours sur Womix, rejoignez moi !');
 
+GO
 INSERT INTO users VALUES ('usr_lXLVeLbaid03vOItRZP11EWdzhqH', 'user1@mail.com', 'login1', 'Arthur', '$2a$11$SKVecUmTEzKAF43qk.QsFuziowabj9HKHpp5UkGacGb./YI6/7Yaa', 1, 'Title 1',0,0,0,0, 'acc_ku784AJalJIkPxQPM6QeLyuPgrno'); --password1
 INSERT INTO users VALUES ('usr_lSELhMwz5sB3mvcOwvHQKzqk5D0t', 'user2@mail.com', 'login2', 'UserAAAA', '$2a$11$40l2odRdREdQrMK75k57euzAHPmHsEGIb7SL8zEnXjzeAdDI1hvAS', 1, 'Title 2',0,0,0,0, 'acc_f7Dtuqn32rAp7ocsonMiK6MAkOoE'); --password2
-INSERT INTO users VALUES ('usr_VpKvQZ6zpEJt9y2ykFptGRX4gaSd', 'mod3@mail.com', 'login3', 'User 3', '$2a$11$40l2odRdREdQrMK75k57euzAHPmHsEGIb7SL8zEnXjzeAdDI1hvAS', 2, 'MegaTropFor',0,0,0,0, "acc_caTaXKCD7D9AgGAQxT9kPOxxxIna"); --password2
-INSERT INTO users VALUES ('usr_8vLHdpRjrqF_RRM2NJcVEFqmZkGt', 'cestmoihaha@me.be', 'coucou', 'Theo Mille', '$2a$11$hbAXocUS0vEih436Lw8ddeaVN21kvdkHDruRyv/CTir2B1ZbJvuXW', 1, 'Verified',0,0,0,0, "acc_8vLHdpRjrqF_RRM2NJcVEFqmZkGt"); --Strong#1
+INSERT INTO users VALUES ('usr_VpKvQZ6zpEJt9y2ykFptGRX4gaSd', 'mod3@mail.com', 'login3', 'User 3', '$2a$11$40l2odRdREdQrMK75k57euzAHPmHsEGIb7SL8zEnXjzeAdDI1hvAS', 2, 'MegaTropFor',0,0,0,0, 'acc_caTaXKCD7D9AgGAQxT9kPOxxxIna'); --password2
+INSERT INTO users VALUES ('usr_8vLHdpRjrqF_RRM2NJcVEFqmZkGt', 'cestmoihaha@me.be', 'coucou', 'Theo Mille', '$2a$11$hbAXocUS0vEih436Lw8ddeaVN21kvdkHDruRyv/CTir2B1ZbJvuXW', 1, 'Verified',0,0,0,0, 'acc_8vLHdpRjrqF_RRM2NJcVEFqmZkGt'); --Strong#1
 
-
-
+GO
 INSERT INTO friends VALUES ('usr_lSELhMwz5sB3mvcOwvHQKzqk5D0t', 'usr_lXLVeLbaid03vOItRZP11EWdzhqH');
 INSERT INTO friends VALUES ('usr_lSELhMwz5sB3mvcOwvHQKzqk5D0t', 'usr_VpKvQZ6zpEJt9y2ykFptGRX4gaSd');
 INSERT INTO friends VALUES ('usr_lXLVeLbaid03vOItRZP11EWdzhqH', 'usr_lSELhMwz5sB3mvcOwvHQKzqk5D0t');
@@ -131,9 +124,16 @@ INSERT INTO friends VALUES ('usr_VpKvQZ6zpEJt9y2ykFptGRX4gaSd', 'usr_lXLVeLbaid0
 INSERT INTO friends VALUES ('usr_lXLVeLbaid03vOItRZP11EWdzhqH', 'usr_8vLHdpRjrqF_RRM2NJcVEFqmZkGt');
 INSERT INTO friends VALUES ('usr_8vLHdpRjrqF_RRM2NJcVEFqmZkGt', 'usr_lXLVeLbaid03vOItRZP11EWdzhqH');
 
-
-
+GO
 INSERT INTO publications(pub_content, pub_date, user_id) VALUES ('content 1', '1990-01-02', 'usr_lXLVeLbaid03vOItRZP11EWdzhqH');
-INSERT INTO publications(pub_content, pub_date, user_id) VALUES ( 'content 3', '1990-01-03', 'usr_lXLVeLbaid03vOItRZP11EWdzhqH');
-INSERT INTO publications(pub_content, pub_date, user_id) VALUES ( 'content 2', '1990-01-02', 'usr_lSELhMwz5sB3mvcOwvHQKzqk5D0t');
+INSERT INTO publications(pub_content, user_id) VALUES ( 'content 3', 'usr_lXLVeLbaid03vOItRZP11EWdzhqH');
+INSERT INTO publications(pub_content, user_id) VALUES ( 'content 2', 'usr_lSELhMwz5sB3mvcOwvHQKzqk5D0t');
 
+GO
+INSERT INTO images (img_path, img_date) VALUES ('img_ZKJAVDJVZDKJHAVZDHVSDZ', '1990-01-02');
+INSERT INTO images (img_path) VALUES ('img_ZIODNMOIAHDMHQZIODNSZQDQ');
+INSERT INTO images (img_path) VALUES ('img_ZLKBAHOLDBCLJZAUBD');
+
+GO
+INSERT INTO publication_elements (img_id, pub_id) VALUES (2, 1);
+INSERT INTO publication_elements (img_id, pub_id) VALUES (3, 1);
