3274f84c9437fb47efa366a88b55f480bf0f90af
DrimoZ <drimozbe@gmail.com>
2023-12-09 13:27:18 +0100

    Add Like Repository from DB

---
+++ Infrastructure/EntityFramework/Repositories/Publications/ILikeRepository.cs
@@ -0,0 +1,11 @@
+using Infrastructure.EntityFramework.DbEntities;
+
+namespace Infrastructure.EntityFramework.Repositories.Publications;
+
+public interface ILikeRepository
+{
+    DbLike Create(DbLike like);
+    bool Delete(int likeId);
+    DbLike FetchById(int likeId);
+    IEnumerable<DbLike> FetchLikesByPublicationId(int pubId);
+}
