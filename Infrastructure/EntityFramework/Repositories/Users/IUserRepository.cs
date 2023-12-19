3d6a8b7e28c5fc2df43ebec213c78fc8bceb3918
DrimoZ <drimozbe@gmail.com>
2023-12-19 19:34:17 +0100

    Fix Account Modification v2

--- Infrastructure/EntityFramework/Repositories/Users/IUserRepository.cs
+++ Infrastructure/EntityFramework/Repositories/Users/IUserRepository.cs
@@ -7,10 +7,10 @@ public interface IUserRepository
     DbUser Create(DbUser user);
     bool Update(DbUser user);
     bool Delete(string id);
-
     DbUser FetchById(string id);
     DbUser FetchByLoginOrMail(string login);
     DbUser FetchByLoginAndMail(string login, string mail);
     DbUser FetchByLogin(string login);
     IEnumerable<DbUser> FetchUsersByFilter(string userIdToIgnore, string nameFilter, int userCount);
+    bool CheckDuplicatedMail(string userId, string mail);
 }
